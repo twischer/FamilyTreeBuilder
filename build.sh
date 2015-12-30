a159fae29eb9b53863bc1c1a73a22bc5b3d5d306
Timo Wischer <twischer@freenet.de>
2015-12-30 17:57:45 +0100

    Use different templates for formating

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
+TEMPLATE="templateFading"
+#TEMPLATE="templateGolden"
+
+
 while [ 1 ]
 do
 	./family2latex.py ./data/family.xml > family.tex
-	pdflatex ./template.tex
+	pdflatex ./${TEMPLATE}.tex
 	read
 done
